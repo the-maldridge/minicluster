9bb96f50c86c5af31ad8b72860094c99427b71f5
Michael Aldridge <aldridge.mac@gmail.com>
2021-01-09 14:20:49 -0800

    Remove matchbox; its effectively abandonware

--- vars.tf
+++ vars.tf
@@ -3,9 +3,3 @@ variable "subnet" {
   description = "Subnet of the minicluster"
   default     = "192.168.32.0/24"
 }
-
-variable "matchbox_domain" {
-  type        = string
-  description = "Matchbox domain"
-  default     = "bootmaster.lan"
-}
