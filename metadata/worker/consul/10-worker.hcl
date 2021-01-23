18d6a92a07d2881e2462f2f2c57a5b227ad0d9d1
Michael Aldridge <aldridge.mac@gmail.com>
2021-01-23 14:16:56 -0800

    Set up metadata for workers

---
+++ metadata/worker/consul/10-worker.hcl
@@ -0,0 +1,2 @@
+datacenter = "minicluster"
+retry_join = ["node1", "node2", "node3"]
--- metadata_server.tf
+++ metadata_server.tf
@@ -3,9 +3,34 @@ data "linuxkit_metadata" "aio" {
 }
 
 resource "local_file" "aio_metadata" {
-  for_each = toset(["00-23-24-5B-85-92", "00-23-24-6C-68-5C", "00-23-24-79-02-CB"])
+  for_each = toset([
+    "00-23-24-5B-85-92",
+    "00-23-24-6C-68-5C",
+    "00-23-24-79-02-CB",
+  ])
 
   content  = data.linuxkit_metadata.aio.json
+  filename = "${path.root}/metaldata_data/${each.value}.userdata"
+
+  file_permission = "0644"
+}
+
+data "linuxkit_metadata" "worker" {
+  base_path = "${path.module}/metadata/worker"
+}
+
+resource "local_file" "worker_metadata" {
+  for_each = toset([
+    "00-23-24-64-1D-59",
+    "D8-CB-8A-20-92-E6",
+    "00-23-24-78-24-39",
+    "00-23-24-76-8B-ED",
+    "00-23-24-76-C6-A3",
+    "00-23-24-79-02-FA",
+    "00-23-24-69-DF-C4",
+  ])
+
+  content  = data.linuxkit_metadata.worker.json
   filename = "${path.root}/metaldata_data/${each.value}.userdata"
 
   file_permission = "0644"
