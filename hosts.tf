b3823db72bfd436374b69072bdf888291180518e
Michael Aldridge <aldridge.mac@gmail.com>
2021-01-23 20:04:16 -0800

    Set up Shoelaces mappings

--- hosts.tf
+++ hosts.tf
@@ -1,15 +1,12 @@
 locals {
   hosts = {
 
-    "00-00-00-00-00-00" = {
-      hostname = "dummy"
-    }
-
     "00-23-24-5B-85-92" = {
       hostname       = "node1"
       private-ipv4   = "192.168.32.10"
       failure-domain = "S1L"
       machine-type   = "m73-tiny"
+      role           = "aio"
     }
 
     "00-23-24-6C-68-5C" = {
@@ -17,6 +14,7 @@ locals {
       private-ipv4   = "192.168.32.11"
       failure-domain = "S1R"
       machine-type   = "m73-tiny"
+      role           = "aio"
     }
 
     "00-23-24-79-02-CB" = {
@@ -24,6 +22,7 @@ locals {
       private-ipv4   = "192.168.32.12"
       failure-domain = "S2L"
       machine-type   = "m73-tiny"
+      role           = "aio"
     }
 
     "00-23-24-64-1D-59" = {
@@ -31,6 +30,7 @@ locals {
       private-ipv4   = "192.168.32.13"
       failure-domain = "S2R"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "D8-CB-8A-20-92-E6" = {
@@ -38,6 +38,7 @@ locals {
       private-ipv4   = "192.168.32.14"
       failure-domain = "S3L"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "00-23-24-78-24-39" = {
@@ -45,6 +46,7 @@ locals {
       private-ipv4   = "192.168.32.15"
       failure-domain = "S3R"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "00-23-24-76-8B-ED" = {
@@ -52,6 +54,7 @@ locals {
       private-ipv4   = "192.168.32.16"
       failure-domain = "S4L"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "00-23-24-76-C6-A3" = {
@@ -59,6 +62,7 @@ locals {
       private-ipv4   = "192.168.32.17"
       failure-domain = "S4R"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "00-23-24-79-02-FA" = {
@@ -66,6 +70,7 @@ locals {
       private-ipv4   = "192.168.32.18"
       failure-domain = "S5L"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
     "00-23-24-69-DF-C4" = {
@@ -73,6 +78,7 @@ locals {
       private-ipv4   = "192.168.32.19"
       failure-domain = "S5R"
       machine-type   = "m73-tiny"
+      role           = "worker"
     }
 
   }
@@ -84,5 +90,15 @@ resource "local_file" "metadata" {
   content = jsonencode(each.value)
 
   filename        = "${path.root}/metaldata_data/${each.key}.json"
+  file_permission = "0644"
+}
+
+resource "local_file" "shoelaces_mappings" {
+  content = yamlencode({
+    networkMaps  = []
+    hostnameMaps = [for host, attrs in local.hosts : { hostname = "${attrs.hostname}.lan", script = { name = "${attrs.role}.ipxe" } }]
+  })
+
+  filename        = abspath(join("/", [path.root, "shoelaces_data", "mappings.yaml"]))
   file_permission = "0644"
 }
