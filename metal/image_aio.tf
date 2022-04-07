366b4d13c7bc39e2917f997cb7bdd4cf7562bf4b
Michael Aldridge <aldridge.mac@gmail.com>
2022-04-07 15:11:41 -0500

    metal: Remove version overrides

--- metal/image_aio.tf
+++ metal/image_aio.tf
@@ -1,9 +1,6 @@
 module "aio_server" {
   source = "../../terraform-linuxkit-resinstack"
 
-  system_version_metadata   = "2cf1db0f0d2c9916b4894318bd76f1c97d8c8f7b"
-  system_metadata_providers = ["metaldata"]
-
   enable_console = true
   enable_sshd    = true
   enable_ntpd    = true
@@ -11,7 +8,6 @@ module "aio_server" {
   consul_server = true
   consul_acl    = "deny"
 
-  nomad_version = "v1.2.0b1r0"
   nomad_server  = true
   nomad_client  = true
   enable_docker = true
--- metal/image_worker.tf
+++ metal/image_worker.tf
@@ -1,14 +1,10 @@
 module "worker" {
   source = "../../terraform-linuxkit-resinstack"
 
-  system_version_metadata   = "2cf1db0f0d2c9916b4894318bd76f1c97d8c8f7b"
-  system_metadata_providers = ["metaldata"]
-
   enable_console = true
   enable_sshd    = true
   enable_ntpd    = true
 
-  nomad_version = "v1.2.0b1r0"
   nomad_client  = true
   enable_docker = true
   nomad_acl     = true
